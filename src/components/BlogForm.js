6aba8da104115bb827b72307b050936ab34646b4
gsaidov <gulomjonsaidov@gmail.com>
2021-03-17 22:34:00 -0700

    Reducing Boilerplate - getFieldProps()

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -35,14 +35,7 @@ function BlogForm(props) {
       <form onSubmit={formik.handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-          />
+          <input type="text" id="name" {...formik.getFieldProps("name")} />
           {formik.touched.name && formik.errors.name ? (
             <div className="error">{formik.errors.name}</div>
           ) : null}
@@ -50,14 +43,7 @@ function BlogForm(props) {
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
+          <input type="email" id="email" {...formik.getFieldProps("email")} />
           {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
           ) : null}
@@ -65,14 +51,7 @@ function BlogForm(props) {
 
         <div className="form-control">
           <label htmlFor="title">Blog Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.title}
-          />
+          <input type="text" id="title" {...formik.getFieldProps("title")} />
           {formik.touched.title && formik.errors.title ? (
             <div className="error">{formik.errors.title}</div>
           ) : null}
